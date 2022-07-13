929f78e5d7d6ac6a9ee4842ed297c8ca9df53d1d
LeejunYing <lytuanvinh1@gmail.com>
2022-07-13 12:03:08 +0700

    Update Voucher ,Categorys ,Paypal

--- routes/categorys.js
+++ routes/categorys.js
@@ -1,11 +1,5 @@
 // @ts-nocheck
 
-let A =[1,2,3]
-
-Array(3).map()
-
-a.map()
-
 const categorysController = require("../controllers/categorysController");
 const router = require("express-promise-router")();
 const {
@@ -17,17 +11,11 @@ const categorys = require("../models/categorys");
 
 router
   .route("/")
-  .get(
-    validator.validateCategoryQuery(validator.schemas.categoryQuerySchema),
-    categorysController.getCategory,
-  )
+  .get(categorysController.getAll)
   .post(
-    // verifyTokenAndAdmin,
+    verifyTokenAndAdmin,
     validator.validateBody(validator.schemas.categorysSchema),
     categorysController.newCategory,
   );
-
-router.route("/all").get(categorysController.getAll);
-router.route("/:key").get(categorysController.getCategoryID);
 
 module.exports = router;
