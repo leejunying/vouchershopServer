ae814093da6afe7179bd979c7eca96f97dffa0ad
LeejunYing <lytuanvinh1@gmail.com>
2022-07-13 12:19:03 +0700

    fix

--- routes/categorys.js
+++ routes/categorys.js
@@ -1,11 +1,5 @@
 // @ts-nocheck
 
-let A =[1,2,3]
-
-Array(3).map()
-
-a.map()
-
 const categorysController = require("../controllers/categorysController");
 const router = require("express-promise-router")();
 const {
